894557b1fcaa9b472f518042ef837665fbc690ad
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-07-20 00:53:37 +0800

    playing with sql

--- posgresqltutorial/playsql.sql
+++ posgresqltutorial/playsql.sql
@@ -28,6 +28,7 @@ BEGIN
     END LOOP;
 END $$;
 
+-- create 3 tables
 CREATE TABLE IF NOT EXISTS address (
     id SERIAL PRIMARY KEY,
     street VARCHAR(100) NOT NULL,
@@ -36,12 +37,45 @@ CREATE TABLE IF NOT EXISTS address (
     postal_code VARCHAR(20) NOT NULL
 );
 
+-- populate address table with random data
+DO $$
+DECLARE
+    i INT := 1;
+BEGIN
+    WHILE i <= 100 LOOP
+        INSERT INTO address (street, city, state, postal_code)
+        VALUES (
+            (SELECT md5(random()::text)),
+            (SELECT md5(random()::text)),
+            (SELECT md5(random()::text)),
+            (SELECT substring(md5(random()::text) from 1 for 20))
+        );
+        i := i + 1;
+    END LOOP;
+END $$;
+
 CREATE TABLE IF NOT EXISTS family (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     relation VARCHAR(50) NOT NULL,
     age INT
 );
+
+-- populate family table with random data
+DO $$
+DECLARE
+    i INT := 1;
+BEGIN
+    WHILE i <= 100 LOOP
+        INSERT INTO family (name, relation, age)
+        VALUES (
+            (SELECT md5(random()::text)),
+            (SELECT md5(random()::text)),
+            floor(random() * 80 + 18)
+        );
+        i := i + 1;
+    END LOOP;
+END $$;
 
 CREATE TABLE IF NOT EXISTS jobs (
     id SERIAL PRIMARY KEY,
@@ -50,12 +84,43 @@ CREATE TABLE IF NOT EXISTS jobs (
     salary DECIMAL(10, 2)
 );
 
+-- populate jobs table with random data
+DO $$
+DECLARE
+    i INT := 1;
+BEGIN
+    WHILE i <= 100 LOOP
+        INSERT INTO jobs (title, company, salary)
+        VALUES (
+            (SELECT md5(random()::text)),
+            (SELECT md5(random()::text)),
+            floor(random() * 1000000)
+        );
+        i := i + 1;
+    END LOOP;
+END $$;
 
+-- using join
+SELECT u.name, u.age, j.title, j.company, a.street, a.city
+FROM users u
+JOIN jobs j ON u.id = j.id
+JOIN address a ON u.id = a.id;
 
-
-
+--using cte (coomon table expression)
+WITH
+    user_jobs AS (
+        SELECT u.name, u.age, j.title, j.company
+        FROM users u
+            JOIN jobs j ON u.id = j.id
+    ),
+    user_address AS (
+        SELECT u.name, u.age, a.street, a.city
+        FROM users u
+            JOIN address a ON u.id = a.id
+    )
+SELECT uj.name, uj.age, uj.title, uj.company, ua.street, ua.city
+FROM user_jobs uj
+    JOIN user_address ua ON uj.name = ua.name;
 SELECT * FROM users
-
-
 
 
