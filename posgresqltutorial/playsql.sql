688965e56904a280f5f6a7948e41186b2773034a
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-07-21 03:53:36 +0800

    add table with foregin keys constraint

--- posgresqltutorial/playsql.sql
+++ posgresqltutorial/playsql.sql
@@ -381,3 +381,39 @@ SELECT customer_id,
        full_name,
        balance
 FROM BankStatements;
+
+
+-- basic foreign key constrains
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS contacts;
+
+CREATE TABLE customers(
+   customer_id INT GENERATED ALWAYS AS IDENTITY,
+   customer_name VARCHAR(255) NOT NULL,
+   PRIMARY KEY(customer_id)
+);
+
+CREATE TABLE contacts(
+   contact_id INT GENERATED ALWAYS AS IDENTITY,
+   customer_id INT,
+   contact_name VARCHAR(255) NOT NULL,
+   phone VARCHAR(15),
+   email VARCHAR(100),
+   PRIMARY KEY(contact_id),
+   CONSTRAINT fk_customer
+      FOREIGN KEY(customer_id) 
+      REFERENCES customers(customer_id)
+);
+
+-- no action example
+INSERT INTO customers(customer_name)
+VALUES('GeeksforGeeks org'),
+      ('Dolphin LLC');       
+       
+INSERT INTO contacts(customer_id, contact_name, phone, email)
+VALUES (1, 'Raju kumar', '(408)-111-1234', 'raju.kumar@geeksforgeeks.org'),
+               (1, 'Raju kumar', '(408)-111-1235', 'raju.kumar@bluebird.dev'),
+               (2, 'Nikhil Aggarwal', '(408)-222-1234', 'nikhil.aggarwalt@geeksforgeeks.org');
+
+select * from customers;
+select * from contacts;
