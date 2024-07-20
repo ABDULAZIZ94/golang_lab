27ef8fd0e064d798a0a6f607583a0c781ed38bd7
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-07-20 18:53:28 +0800

    play with sql

--- toilet sql files/getoverviewdata.sql
+++ toilet sql files/getoverviewdata.sql
@@ -8,6 +8,55 @@ JOIN DEVICE_TYPES ON DEVICE_TYPES.DEVICE_TYPE_ID = DEVICES.DEVICE_TYPE_ID
 JOIN TOILET_INFOS ON TOILET_INFOS.TOILET_INFO_ID = DEVICE_PAIRS.TOILET_INFO_ID  
 WHERE DEVICE_PAIRS.TOILET_INFO_ID = '0a38e4d1-f9b9-4cb2-648f-20e0ac269984'
 
+-- total user reaction for all toilet
+-- ADDITIONAL DATA FOR NEW FP
+SELECT
+    COUNT(
+        CASE
+            WHEN ur.reaction = '1' THEN 1
+        END
+    ) AS happy,
+    COUNT(
+        CASE
+            WHEN ur.reaction = '2' THEN 1
+        END
+    ) AS satisfied,
+    COUNT(
+        CASE
+            WHEN ur.reaction = '3' THEN 1
+        END
+    ) AS not_satisfied,
+    COUNT(
+        CASE
+            WHEN ur.reaction = '4' THEN 1
+        END
+    ) AS not_happy,
+    COUNT(
+        CASE
+            WHEN ur.complaint = '1' THEN 1
+        END
+    ) AS smelly_toilet,
+    COUNT(
+        CASE
+            WHEN ur.complaint = '2' THEN 1
+        END
+    ) AS out_of_supplies,
+    COUNT(
+        CASE
+            WHEN ur.complaint = '3' THEN 1
+        END
+    ) AS wet_floor,
+    COUNT(
+        CASE
+            WHEN ur.complaint = '4' THEN 1
+        END
+    ) AS plumbing_issues
+FROM user_reactions ur
+
+select * from user_reactions
+
+
+-- user reaction towards a toilet identified by toilet id
 -- ADDITIONAL DATA FOR NEW FP
 SELECT COUNT(CASE WHEN ur.reaction = '1' THEN 1 END) AS happy,  
 COUNT(CASE WHEN ur.reaction = '2' THEN 1 END) AS satisfied,  
@@ -29,6 +78,17 @@ AND (TIMESTAMP) >= TO_TIMESTAMP('  2024-07-01   16:00:00','YYYY-MM-DD HH24:MI:SS
 -- temporary fix mbk cleaner issue from male toiles 
 SELECT LOCATION_ID FROM TOILET_INFOS
 WHERE TOILET_INFO_ID ='0a38e4d1-f9b9-4cb2-648f-20e0ac269984'
+
+select * from locations
+
+select * from toilet_infos
+
+SELECT ti.LOCATION_ID, loc.location_name
+FROM TOILET_INFOS as ti
+JOIN LOCATIONS as loc
+    ON  ti.location_id = loc. location_id
+WHERE
+    TOILET_INFO_ID = '0a38e4d1-f9b9-4cb2-648f-20e0ac269984'
 
 -- get overview card data
 WITH DEVICE_LIST AS ( 
