3cc424ecea967ef93f122edf61c3541a8511040a
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-07-22 00:22:56 +0800

    add rank, dense rank

--- posgresqltutorial/playsql.sql
+++ posgresqltutorial/playsql.sql
@@ -631,3 +631,151 @@ $$;
 -- test stored procedure
 call transfer(1, 2, 1000);
 SELECT * FROM accounts;
+
+
+-- create table for testing trigger
+CREATE TABLE COMPANY(
+   ID INT PRIMARY KEY NOT NULL,
+   NAME TEXT NOT NULL,
+   AGE INT NOT NULL,
+   ADDRESS CHAR(50),
+   SALARY REAL
+);
+
+CREATE TABLE AUDIT(
+   EMP_ID INT NOT NULL,
+   ENTRY_DATE TEXT NOT NULL
+);
+
+-- create trigger
+CREATE OR REPLACE FUNCTION auditlog() RETURNS TRIGGER AS $$
+BEGIN
+   INSERT INTO AUDIT(EMP_ID, ENTRY_DATE) VALUES (NEW.ID, current_timestamp);
+   RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- bind trigger
+CREATE TRIGGER example_trigger AFTER
+INSERT ON COMPANY
+FOR EACH ROW EXECUTE FUNCTION auditlog();
+
+-- insert data and verify that trigegr working
+INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)
+VALUES (1, 'Raju', 25, 'New-Delhi', 33000.00 );
+
+SELECT * FROM COMPANY;
+
+select * from audit
+
+
+-- windows function
+CREATE TABLE product_groups (
+	group_id serial PRIMARY KEY,
+	group_name VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE products (
+	product_id serial PRIMARY KEY,
+	product_name VARCHAR (255) NOT NULL,
+	price DECIMAL (11, 2),
+	group_id INT NOT NULL,
+	FOREIGN KEY (group_id) REFERENCES product_groups (group_id)
+);
+
+INSERT INTO product_groups (group_name)
+VALUES
+	('Smartphone'),
+	('Laptop'),
+	('Tablet');
+
+INSERT INTO products (product_name, group_id,price)
+VALUES
+	('Microsoft Lumia', 1, 200),
+	('HTC One', 1, 400),
+	('Nexus', 1, 500),
+	('iPhone', 1, 900),
+	('HP Elite', 2, 1200),
+	('Lenovo Thinkpad', 2, 700),
+	('Sony VAIO', 2, 700),
+	('Dell Vostro', 2, 800),
+	('iPad', 3, 700),
+	('Kindle Fire', 3, 150),
+	('Samsung Galaxy Tab', 3, 200);
+
+-- test use aggregate function
+SELECT
+	AVG (price)
+FROM
+	products;
+
+
+-- inner join
+SELECT
+	group_name,
+	AVG (price)
+FROM
+	products
+INNER JOIN product_groups USING (group_id)
+GROUP BY
+	group_name;
+
+
+-- use partition
+-- in this query avg works as window function
+SELECT
+	product_name,
+	price,
+	group_name,
+	AVG (price) OVER (
+	   PARTITION BY group_name
+	)
+FROM
+	products
+	INNER JOIN 
+		product_groups USING (group_id);
+
+
+-- use row number
+SELECT
+	product_name,
+	group_name,
+	price,
+	ROW_NUMBER () OVER (
+		PARTITION BY group_name
+		ORDER BY
+			price
+	)
+FROM
+	products
+INNER JOIN product_groups USING (group_id);
+
+
+-- rank
+SELECT
+	product_name,
+	group_name,
+  price,
+	RANK () OVER (
+		PARTITION BY group_name
+		ORDER BY
+			price
+	)
+FROM
+	products
+INNER JOIN product_groups USING (group_id);
+
+
+-- dense rank
+SELECT
+	product_name,
+	group_name,
+	price,
+	DENSE_RANK () OVER (
+		PARTITION BY group_name
+		ORDER BY
+			price
+	)
+FROM
+	products
+INNER JOIN product_groups USING (group_id);
