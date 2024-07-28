eff1ee19e102fa3afa45871be104452a6e87551e
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-07-28 19:33:47 +0800

    fist successful table returning function

--- toilet sql files/analytics.sql
+++ toilet sql files/analytics.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b289eb4bc6d6e1ce5cd2728b10acc21b0f01e66861111f067b033d773a219641
-size 9491
+oid sha256:3171a9cd813afa0f4d3bc329ef3278ec6230ca51a6ee35829661a32f1d49a784
+size 8793
---
+++ toilet sql files/testfunction.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2826b95d73657291a6848240f6b6a0c1e6b39f635b11d9e80a55d2e6542b22e2
+size 3670
