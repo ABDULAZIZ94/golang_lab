9d4c046c42edf809903f41dfb2fdf80decea2b0c
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2024-08-05 10:56:18 +0800

    add .env and add to gitignore

--- awsuploadtest/awss3.go
+++ awsuploadtest/awss3.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,19 +12,26 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/joho/godotenv"
 )
 
 // Here, you can choose the region of your bucket
 func main() {
-	region := "ap-southeast-1"
+	if err := godotenv.Load(".env"); err != nil {
+		log.Print("No .env file found")
+	}
+
+	aws_id := os.Getenv("AWS_ID")
+	aws_region := os.Getenv("AWS_REGION")
+	aws_secret := os.Getenv("AWS_SECRET")
 
 	sess, err := session.NewSession(&aws.Config{
 		Credentials: credentials.NewStaticCredentials(
-			"AKIAUM2TD2VTILTPLCUU",
-			"uPzvajoeEDxAUaoHChKZO7subLsPl0J9zn24SFej",
+			aws_id,
+			aws_secret,
 			"", // a token will be created when the session it's used.
 		),
-		Region: aws.String(region),
+		Region: aws.String(aws_region),
 	})
 	if err != nil {
 		fmt.Println("Error creating session:", err)
